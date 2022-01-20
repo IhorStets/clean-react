a9b3fd9f299baef55331339a9e31a6669c605a66
IhorStets <stets237@gmail.com>
2022-01-20 12:31:32 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 const App = () => {
-  return (
-      <div>
 
-      </div>
-  );
+    let [users, setUsers] = useState([]);
+    let [posts, setPosts] = useState([]);
+    let [comments, setComments] = useState([]);
+
+    useEffect(() => {
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                setUsers(value)
+            });
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(value => {
+                setPosts(value)
+            });
+
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(value => value.json())
+            .then(value => {
+                setComments(value)
+            });
+
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    );
 };
 
 export default App;
